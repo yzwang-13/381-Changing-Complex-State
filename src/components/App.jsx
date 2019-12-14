6528b540bef3f022adeb330a62067d5ce3a644d9
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-14 14:55:47 +1100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullNmae] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    console.log(event.target.name);
+    const { name, value } = event.target;
+    setFullNmae(
+      name === "fName"
+        ? { fName: value, lName: fullName.lName }
+        : { fName: fullName.fName, lName: value }
+    );
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          placeholder="First Name"
+          value={fullName.fName}
+          onChange={handleChange}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          value={fullName.lName}
+          onChange={handleChange}
+        />
         <button>Submit</button>
       </form>
     </div>
   );
 }
+
+// // previous method solotion
+// function App() {
+//   const [fName, setfNmae] = useState("");
+//   const [lName, setlNmae] = useState("");
+
+//   function changeFname(event) {
+//     setfNmae(event.target.value);
+//   }
+
+//   function changeLname(event) {
+//     setlNmae(event.target.value);
+//   }
+//   return (
+//     <div className="container">
+//       <h1>
+//         Hello {fName} {lName}
+//       </h1>
+//       <form>
+//         <input
+//           name="fName"
+//           placeholder="First Name"
+//           value={fName}
+//           onChange={changeFname}
+//         />
+//         <input
+//           name="lName"
+//           placeholder="Last Name"
+//           value={lName}
+//           onChange={changeLname}
+//         />
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 export default App;
